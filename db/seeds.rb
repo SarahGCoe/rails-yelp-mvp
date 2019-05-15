25cde73adcbd2044120139eaa65d27ae611b9d53
Sarah GHANIMI <sarah.ghanimi@gmail.com>
2019-05-15 14:51:29 +0200

    restaurant and reviews done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '01 23 45 67 89',
+    category:  'chinese',
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '01 23 45 67 89',
+    category:  'italian',
+  },
+  {
+    name:         'LonelyPlanet',
+    address:      '16 rue du matin',
+    phone_number: '01 23 45 67 89',
+    category:  'japanese',
+  },
+  {
+    name:         'FavStar',
+    address:      '64564 rue du matin',
+    phone_number: '01 23 45 67 89',
+    category:  'french',
+  },
+  {
+    name:         'La Tripletta',
+    address:      '14 rue du loup',
+    phone_number: '01 23 45 67 89',
+    category:  'belgian',
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
